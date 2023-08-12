2de152024807979d84dc2741dc6288dd9f4d29fc
Yukti Joshi <106856890+Yuktijoshi20@users.noreply.github.com>
2023-08-12 22:45:14 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Anagram {
+    static void approach1(String wordArr[]){
+    String words[]=wordArr.clone();
+    int index=0;
+    for(String word:words){
+        char arr[]=word.toCharArray();
+        Arrays.sort(arr);
+        words[index]=String.valueOf(arr);
+        index++;
+
+    }
+
+    for(int i=0;i<words.length;i++){
+        if((i<words.length-1) && words[i].equals(words[i+1])){
+            System.out.println(wordArr[i] +" "+wordArr[i+1]);
+        }
+    }
+    }
+    public static void main(String[] args) {
+        String wordArr[]={"cat","act","dog","god","hen"};
+        approach1(wordArr);
+    }
+}
